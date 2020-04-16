acdbd885dd664249eb664b0384e91197a5e6f894
Bruno <brunohjs@gmail.com>
2020-04-16 16:19:16 -0300

    Remoção de arquivo desnecessário

--- intents/boas_vindas.js
+++
@@ -1,9 +0,0 @@
-module.exports = df => {
-    df.intent('Boas vindas', conv => {
-        console.log(conv)
-        return new Promise(async (resolve, reject) => {
-            conv.ask('Teste');
-            return resolve();
-        })
-    })
-}
